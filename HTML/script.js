8eb4fa64972b66ede7d903b2e58787b4963c2bf3
Milo Burke <miloburke98@gmail.com>
2025-06-26 09:22:44 -0400

    attempt at using the DOM

---
+++ HTML/script.js
@@ -0,0 +1,9 @@
+const spanGandalf = document.querySelector("#gandalf")
+const spanTest = document.querySelector("#test")
+
+
+spanGandalf.style.backgroundColor = "red"
+spnTest.style.backgroundColor = "red"
+
+
+// This didn't work - ask for help on this one maybe. 
